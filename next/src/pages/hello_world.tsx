0aeaa56128cb3faa67ca95b7f218be34b5c9108d
t-hirata74 <t.hirata.0463@gmail.com>
2024-04-05 17:57:50 +0900

    simplebuttonのサンプル機能を追加2

--- next/src/pages/hello_world.tsx
+++ next/src/pages/hello_world.tsx
@@ -2,11 +2,15 @@ import type { NextPage } from 'next'
 import SimpleButton from '@/components/SimpleButton'
 
 const HelloWorld: NextPage = () => {
+  const handleOnClick = () => {
+    console.log('Clicked from hello_world')
+  }
+
   return (
     <>
       <h1>Title</h1>
       <p>content</p>
-      <SimpleButton text={'From HelloWorld'} />
+      <SimpleButton text={'From HelloWorld'} onClick={handleOnClick} />
     </>
   )
 }
