061913a81c54ffb2f882a06e9accfbf582275ff0
t-hirata74 <t.hirata.0463@gmail.com>
2024-04-08 23:28:11 +0900

    bulletの検知により、includesを追加

--- rails/app/controllers/api/v1/articles_controller.rb
+++ rails/app/controllers/api/v1/articles_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ArticlesController < Api::V1::BaseController
   def index
-    articles = Article.published.order(created_at: :desc).page(params[:page] || 1).per(10)
+    articles = Article.published.order(created_at: :desc).page(params[:page] || 1).per(10).includes([:user])
     render json: articles
   end
 
