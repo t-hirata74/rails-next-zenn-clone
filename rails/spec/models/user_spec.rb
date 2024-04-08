bb33ee4cd4b1c5ad5d1023e19480522b5a0b46b9
t-hirata74 <t.hirata.0463@gmail.com>
2024-04-08 15:29:12 +0900

    user_specのテストコードを追加

--- rails/spec/models/user_spec.rb
+++ rails/spec/models/user_spec.rb
@@ -1,5 +1,12 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "factoryのデフォルト設定に従った場合" do
+    let(:user) { create(:user) }
+
+    it "認証済みの user レコードを正常に新規作成できる" do
+      expect(user).to be_valid
+      expect(user).to be_confirmed
+    end
+  end
 end
