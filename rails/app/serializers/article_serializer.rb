c059dff569a422f8e1b512896fdd9b868bc48640
t-hirata74 <t.hirata.0463@gmail.com>
2024-04-08 19:08:07 +0900

    belongs_to :user, serializer: UserSerializerを追記

--- rails/app/serializers/article_serializer.rb
+++ rails/app/serializers/article_serializer.rb
@@ -1,5 +1,6 @@
 class ArticleSerializer < ActiveModel::Serializer
   attributes :id, :title, :content, :created_at, :from_today
+  belongs_to :user, serializer: UserSerializer
 
   def created_at
     object.created_at.strftime("%Y/%m/%d")
