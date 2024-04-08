9fcfac5275777bb097dea74532f10cad06da7ac4
t-hirata74 <t.hirata.0463@gmail.com>
2024-04-08 19:04:09 +0900

    articles#showアクションの追加

--- rails/app/controllers/api/v1/articles_controller.rb
+++ rails/app/controllers/api/v1/articles_controller.rb
@@ -1,2 +1,6 @@
-class Api::V1::ArticlesController < ApplicationController
+class Api::V1::ArticlesController < Api::V1::BaseController
+  def show
+    article = Article.published.find(params[:id])
+    render json: article
+  end
 end
