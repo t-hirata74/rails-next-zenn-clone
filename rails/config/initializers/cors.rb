780dad142a482c27a90cdbd251749c4dd4408fe2
t-hirata74 <t.hirata.0463@gmail.com>
2024-04-09 22:07:06 +0900

    CORS設定の修正

--- rails/config/initializers/cors.rb
+++ rails/config/initializers/cors.rb
@@ -11,6 +11,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
     resource "*",
              headers: :any,
+             expose: ['access-token', 'uid','client'], # 認証情報をNext.js側で取得できるようにする
              methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
