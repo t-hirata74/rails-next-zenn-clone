52d68468fa236e438c8f993eced4e33eb25ab6bc
t-hirata74 <t.hirata.0463@gmail.com>
2024-04-10 23:06:31 +0900

    ユーザー認証APIの実装

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -4,7 +4,12 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get "health_check", to: "health_check#index"
+      
       mount_devise_token_auth_for "User", at: "auth"
+      
+      namespace :user do
+        resource :confirmations, only: [:update]
+      end
 
       namespace :current do
         resource :user, only: [:show]
