d53d8d79c1ed3e64133e03376e0fcf776f766a98
t-hirata74 <t.hirata.0463@gmail.com>
2024-04-10 20:09:29 +0900

    新規アカウント登録時にnameを保存

--- rails/app/controllers/application_controller.rb
+++ rails/app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
   include DeviseHackFakeSession # Session回避用の concerns
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    # サインアップ時のストロングパラメーターにnameカラムを追加する
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
